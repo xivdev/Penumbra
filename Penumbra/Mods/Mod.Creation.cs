ee87098386de2dd0c26c5a6a19084331644a96e1
Ottermandias <nickroederspam+git1@gmail.com>
2022-05-28 16:18:47 +0200

    Fix all files getting added to the default option on import.

--- Penumbra/Mods/Mod.Creation.cs
+++ Penumbra/Mods/Mod.Creation.cs
@@ -122,6 +122,7 @@ public partial class Mod
     internal static void CreateDefaultFiles( DirectoryInfo directory )
     {
         var mod = new Mod( directory );
+        mod.Reload( out _ );
         foreach( var file in mod.FindUnusedFiles() )
         {
             if( Utf8GamePath.FromFile( new FileInfo( file.FullName ), directory, out var gamePath, true ) )
