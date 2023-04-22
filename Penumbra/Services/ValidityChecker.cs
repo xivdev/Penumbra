5ba455fe71d87fe33c914549677dcd896d5b3215
Ottermandias <nickroederspam+git1@gmail.com>
2023-04-22 22:01:39 +0200

    More stupid.

--- Penumbra/Services/ValidityChecker.cs
+++ Penumbra/Services/ValidityChecker.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using Dalamud.Interface.Internal.Notifications;
 using Dalamud.Plugin;
