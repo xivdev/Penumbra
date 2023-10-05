8b5437c2c70237b786716c1fd82cf89361de4442
Ottermandias <nickroederspam+git1@gmail.com>
2023-10-05 17:39:38 +0200

    Remove CS temp fix.

--- Penumbra/Interop/MaterialPreview/LiveMaterialPreviewer.cs
+++ Penumbra/Interop/MaterialPreview/LiveMaterialPreviewer.cs
@@ -80,8 +80,7 @@ public sealed unsafe class LiveMaterialPreviewer : LiveMaterialPreviewerBase
 
         for (var i = 0; i < _shaderPackage->MaterialElementCount; ++i)
         {
-            // TODO fix when CS updated
-            ref var parameter = ref ((ShaderPackage.MaterialElement*) ((byte*)_shaderPackage + 0x98))[i];
+            ref var parameter = ref _shaderPackage->MaterialElementsSpan[i];
             if (parameter.CRC == parameterCrc)
             {
                 if ((parameter.Offset & 0x3) != 0
