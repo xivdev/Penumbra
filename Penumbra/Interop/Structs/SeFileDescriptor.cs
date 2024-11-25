9822ab4128dedc12d9c5cd08af502dce3f06bb53
Ottermandias <nickroederspam+git1@gmail.com>
2024-11-25 16:59:07 +0100

    Add some debug helper output for SeFileDescriptor.

--- Penumbra/Interop/Structs/SeFileDescriptor.cs
+++ Penumbra/Interop/Structs/SeFileDescriptor.cs
@@ -1,3 +1,6 @@
+using Dalamud.Memory;
+using Penumbra.String.Functions;
+
 namespace Penumbra.Interop.Structs;
 
 [StructLayout(LayoutKind.Explicit)]
@@ -14,4 +17,18 @@ public unsafe struct SeFileDescriptor
 
     [FieldOffset(0x70)]
     public char Utf16FileName;
+
+    public FFXIVClientStructs.FFXIV.Client.System.Resource.Handle.ResourceHandle* CsResourceHandele
+        => (FFXIVClientStructs.FFXIV.Client.System.Resource.Handle.ResourceHandle*)ResourceHandle;
+
+    public string FileName
+    {
+        get
+        {
+            fixed (char* ptr = &Utf16FileName)
+            {
+                return MemoryMarshal.CreateReadOnlySpanFromNullTerminated(ptr).ToString();
+            }
+        }
+    }
 }
