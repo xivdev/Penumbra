0afcae45046c5e4b25b59757296500aa92d4aae2
Ottermandias <nickroederspam+git1@gmail.com>
2025-04-05 18:49:30 +0200

    Run API redraws on framework.

--- Penumbra/Api/Api/RedrawApi.cs
+++ Penumbra/Api/Api/RedrawApi.cs
@@ -1,23 +1,32 @@
 using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Plugin.Services;
 using OtterGui.Services;
 using Penumbra.Api.Enums;
 using Penumbra.Interop.Services;
 
 namespace Penumbra.Api.Api;
 
-public class RedrawApi(RedrawService redrawService) : IPenumbraApiRedraw, IApiService
+public class RedrawApi(RedrawService redrawService, IFramework framework) : IPenumbraApiRedraw, IApiService
 {
     public void RedrawObject(int gameObjectIndex, RedrawType setting)
-        => redrawService.RedrawObject(gameObjectIndex, setting);
+    {
+        framework.RunOnFrameworkThread(() => redrawService.RedrawObject(gameObjectIndex, setting));
+    }
 
     public void RedrawObject(string name, RedrawType setting)
-        => redrawService.RedrawObject(name, setting);
+    {
+        framework.RunOnFrameworkThread(() => redrawService.RedrawObject(name, setting));
+    }
 
     public void RedrawObject(IGameObject? gameObject, RedrawType setting)
-        => redrawService.RedrawObject(gameObject, setting);
+    {
+        framework.RunOnFrameworkThread(() => redrawService.RedrawObject(gameObject, setting));
+    }
 
     public void RedrawAll(RedrawType setting)
-        => redrawService.RedrawAll(setting);
+    {
+        framework.RunOnFrameworkThread(() => redrawService.RedrawAll(setting));
+    }
 
     public event GameObjectRedrawnDelegate? GameObjectRedrawn
     {
