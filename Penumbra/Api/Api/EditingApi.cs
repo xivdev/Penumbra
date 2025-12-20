73f02851a64e2a0ea9447173a7981d098a38bac1
Ottermandias <nickroederspam+git1@gmail.com>
2025-12-20 21:51:34 +0100

    Cherry pick API support for other block compression types from Luna branch.

--- Penumbra/Api/Api/EditingApi.cs
+++ Penumbra/Api/Api/EditingApi.cs
@@ -19,6 +19,12 @@ public class EditingApi(TextureManager textureManager) : IPenumbraApiEditing, IA
             TextureType.Bc3Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC3,    mipMaps, false, inputFile, outputFile),
             TextureType.Bc7Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC7,    mipMaps, true,  inputFile, outputFile),
             TextureType.Bc7Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC7,    mipMaps, false, inputFile, outputFile),
+            TextureType.Bc1Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC1,    mipMaps, true,  inputFile, outputFile),
+            TextureType.Bc1Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC1,    mipMaps, false, inputFile, outputFile),
+            TextureType.Bc4Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC4,    mipMaps, true,  inputFile, outputFile),
+            TextureType.Bc4Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC4,    mipMaps, false, inputFile, outputFile),
+            TextureType.Bc5Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC5,    mipMaps, true,  inputFile, outputFile),
+            TextureType.Bc5Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC5,    mipMaps, false, inputFile, outputFile),
             _                   => Task.FromException(new Exception($"Invalid input value {textureType}.")),
         };
 
@@ -36,6 +42,12 @@ public class EditingApi(TextureManager textureManager) : IPenumbraApiEditing, IA
             TextureType.Bc3Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC3,    mipMaps, false, new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
             TextureType.Bc7Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC7,    mipMaps, true,  new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
             TextureType.Bc7Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC7,    mipMaps, false, new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
+            TextureType.Bc1Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC1,    mipMaps, true,  new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
+            TextureType.Bc1Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC1,    mipMaps, false, new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
+            TextureType.Bc4Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC4,    mipMaps, true,  new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
+            TextureType.Bc4Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC4,    mipMaps, false, new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
+            TextureType.Bc5Tex  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC5,    mipMaps, true,  new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
+            TextureType.Bc5Dds  => textureManager.SaveAs(CombinedTexture.TextureSaveType.BC5,    mipMaps, false, new BaseImage(), outputFile, rgbaData, width, rgbaData.Length / 4 / width),
             _                   => Task.FromException(new Exception($"Invalid input value {textureType}.")),
         };
     // @formatter:on
