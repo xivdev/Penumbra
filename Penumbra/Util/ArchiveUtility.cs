d3cc5b0b58646996ad5b3a183e8b8280614d3df3
Exter-N <exter-n@exter-n.fr>
2026-01-27 04:44:42 +0100

    Fix SharpCompress reading regression

--- Penumbra/Util/ArchiveUtility.cs
+++ Penumbra/Util/ArchiveUtility.cs
@@ -1,5 +1,6 @@
 using SharpCompress.Archives;
 using SharpCompress.Common;
+using SharpCompress.Readers;
 
 namespace Penumbra.Util;
 
@@ -25,5 +26,34 @@ public static class ArchiveUtility
     {
         Directory.CreateDirectory(directoryPath);
         ArchiveFactory.WriteToDirectory(filePath, directoryPath, ExtractionOptions);
+    }
+
+    public static void ForEachEntry(IArchive archive, Action<ReaderShim> action)
+    {
+        if (archive.IsSolid || archive.Type is ArchiveType.SevenZip)
+        {
+            var reader = archive.ExtractAllEntries();
+
+            while (reader.MoveToNextEntry())
+                action(new(reader.Entry, reader.OpenEntryStream));
+        }
+        else
+        {
+            foreach (var entry in archive.Entries)
+                action(new(entry, entry.OpenEntryStream));
+        }
+    }
+
+    /// <summary> This shim imitates the parts of <see cref="IReader"/> that are used throughout the importers. </summary>
+    public readonly record struct ReaderShim(IEntry Entry, Func<Stream> OpenEntryStream)
+    {
+        public void WriteEntryToDirectory(string directory)
+        {
+            var path = Path.Combine(directory, Entry.Key!);
+            Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+            using var e = OpenEntryStream();
+            using var f = File.Open(path, FileMode.Create, FileAccess.Write);
+            e.CopyTo(f);
+        }
     }
 }
