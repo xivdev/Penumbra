5bbccc8248b78185ee84097533f8ac3175438555
Exter-N <exter-n@exter-n.fr>
2026-03-12 06:35:30 +0100

    Separate .tex and .atex in texture save dialog

--- Penumbra/UI/FileEditing/Textures/CombiningTextureEditor.Interface.cs
+++ Penumbra/UI/FileEditing/Textures/CombiningTextureEditor.Interface.cs
@@ -319,7 +319,7 @@ public partial class CombiningTextureEditor
     private void OpenSaveAsDialog(string defaultExtension)
     {
         var fileName = Path.GetFileNameWithoutExtension(_left.Path.Length > 0 ? _left.Path : _right.Path);
-        _fileDialog.OpenSavePicker("Save Texture as TEX, DDS, PNG or TGA...", "Textures{.png,.dds,.tex,.atex,.tga},.tex{.tex,.atex},.dds,.png,.tga", fileName,
+        _fileDialog.OpenSavePicker("Save Texture as TEX, DDS, PNG or TGA...", "Textures{.png,.dds,.tex,.atex,.tga},.tex,.atex,.dds,.png,.tga", fileName,
             defaultExtension,
             (a, b) =>
             {
