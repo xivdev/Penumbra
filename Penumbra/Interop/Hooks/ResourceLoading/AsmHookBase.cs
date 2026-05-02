97a9e005aaede303a78bf2628c1274aa6bd3e4c9
pmgr <26606291+pmgr@users.noreply.github.com>
2026-05-02 23:08:30 +0100

    Add AsmHookBase and ModelSafetyCheck; also adapt PapRewriter to use it

---
+++ Penumbra/Interop/Hooks/ResourceLoading/AsmHookBase.cs
@@ -0,0 +1,94 @@
+﻿using Dalamud.Hooking;
+using Iced.Intel;
+using static Iced.Intel.AssemblerRegisters;
+
+namespace Penumbra.Interop.Hooks.ResourceLoading;
+
+public abstract class AsmHookBase(PeSigScanner sigScanner) : IDisposable
+{
+    protected readonly Dictionary<nint, AsmHook> Hooks            = [];
+    protected readonly List<nint>                NativeAllocCaves = [];
+    
+    protected PeSigScanner SigScanner { get; } = sigScanner;
+
+
+    protected AsmHook Setup(string hookName, nint hookAddress, byte[] bytecode, AsmHookBehaviour behaviour = AsmHookBehaviour.ExecuteFirst)
+    {
+        var hook = new AsmHook(hookAddress, bytecode, hookName, behaviour);
+
+        Hooks.Add(hookAddress, hook);
+        
+        return hook;
+    }
+    
+    
+    protected static AssemblerRegister64 GetRegister64(Register reg)
+        => reg switch
+        {
+            Register.RAX => rax,
+            Register.RCX => rcx,
+            Register.RDX => rdx,
+            Register.RBX => rbx,
+            Register.RSP => rsp,
+            Register.RBP => rbp,
+            Register.RSI => rsi,
+            Register.RDI => rdi,
+            Register.R8  => r8,
+            Register.R9  => r9,
+            Register.R10 => r10,
+            Register.R11 => r11,
+            Register.R12 => r12,
+            Register.R13 => r13,
+            Register.R14 => r14,
+            Register.R15 => r15,
+            _            => throw new ArgumentOutOfRangeException(nameof(reg), reg, "Unsupported register."),
+        };
+    
+    protected static byte[] AssembleToBytes(Assembler assembler)
+    {
+        using var stream = new MemoryStream();
+        var       writer = new StreamCodeWriter(stream);
+        assembler.Assemble(writer, 0);
+        return stream.ToArray();
+    }
+    
+    protected unsafe nint NativeAllocCave(nuint size)
+    {
+        var caveLoc = (nint)NativeMemory.Alloc(size);
+        NativeAllocCaves.Add(caveLoc);
+
+        return caveLoc;
+    }
+    
+    protected static unsafe void NativeFree(nint mem)
+        => NativeMemory.Free((void*)mem);
+
+
+    public virtual void Enable()
+    {
+        foreach (var hook in Hooks.Values)
+            hook.Enable();
+    }
+    
+    public virtual void Disable()
+    {
+        foreach (var hook in Hooks.Values)
+            hook.Disable();
+    }
+    
+    public virtual void Dispose()
+    {
+        foreach (var hook in Hooks.Values)
+        {
+            hook.Disable();
+            hook.Dispose();
+        }
+
+        Hooks.Clear();
+
+        foreach (var mem in NativeAllocCaves)
+            NativeFree(mem);
+
+        NativeAllocCaves.Clear();
+    }
+}
---
+++ Penumbra/Interop/Hooks/ResourceLoading/ModelSafetyCheck.cs
@@ -0,0 +1,41 @@
+﻿using Dalamud.Hooking;
+using Iced.Intel;
+using Penumbra.GameData;
+using static Iced.Intel.AssemblerRegisters;
+
+namespace Penumbra.Interop.Hooks.ResourceLoading;
+
+//public sealed class PapRewriter(PeSigScanner sigScanner, PapRewriter.PapResourceHandlerPrototype papResourceHandler) : IDisposable
+public sealed class ModelSafetyCheck(PeSigScanner sigScanner) : AsmHookBase(sigScanner)
+{
+    public void Setup()
+    { 
+        const string sig = "48 8B 41 10 4C 39 88 C8 00 00 00";
+        const string name = "ModelSafetyCheck";
+        if (!SigScanner.TryScanText(sig, out var address))
+            throw new Exception($"Signature for {name} [{sig}] could not be found.");
+
+        var assembler = new Assembler(32);
+        
+        var labelNullPath = assembler.CreateLabel("null_path");
+        var labelEnd      = assembler.CreateLabel("end");
+        
+        // Test if rcx is null; effectively if (model->Materials[i] == nullptr) -> jump to the null_path label
+        assembler.test(rcx, rcx);
+        assembler.jz(labelNullPath);
+        
+        // Original code we wrote over; if (model->Materials[i] != nullptr), it's safe to run it, then jump over the null_path case
+        assembler.mov(rax, __qword_ptr[rcx + 0x10]);
+        assembler.cmp(__qword_ptr[rax + 0xc8], r9);
+        assembler.jmp(labelEnd);
+        
+        // model->Materials[i] is null; we zero out ZF via test rsp, rsp, which assumes rsp != 0. Safe assumption, I'd say, before it fucks me
+        assembler.Label(ref labelNullPath);
+        assembler.test(rsp, rsp);
+        
+        // Easy label to jump over the test shenanigans, preserving the original cmp flags
+        assembler.Label(ref labelEnd);
+        
+        base.Setup(name, address, AssembleToBytes(assembler), AsmHookBehaviour.DoNotExecuteOriginal).Enable();
+    }
+}
