bb381d21de3b2a8ab3ce4037c423b8a511d62b51
Ottermandias <nickroederspam+git1@gmail.com>
2026-05-08 01:05:17 +0200

    Add hook for base clip association.

---
+++ Penumbra/Interop/Hooks/Animation/GetClipResources.cs
@@ -0,0 +1,58 @@
+using FFXIVClientStructs.FFXIV.Client.System.Scheduler.Clip;
+using Luna;
+using Penumbra.CrashHandler.Buffers;
+using Penumbra.GameData;
+using Penumbra.GameData.Interop;
+using Penumbra.Interop.PathResolving;
+using Penumbra.Services;
+
+namespace Penumbra.Interop.Hooks.Animation;
+
+public sealed unsafe class GetClipResources : FastHook<GetClipResources.Delegate>
+{
+    private readonly delegate* unmanaged<BaseClip*, Actor> _clipGameObject;
+    private readonly GameState                             _state;
+    private readonly CollectionResolver                    _collectionResolver;
+    private readonly CrashHandlerService                   _crashHandler;
+
+    public GetClipResources(HookManager hooks, GameState state, CollectionResolver collectionResolver, ObjectManager objects,
+        CrashHandlerService crashHandler)
+    {
+        _state              = state;
+        _collectionResolver = collectionResolver;
+        _crashHandler       = crashHandler;
+        _clipGameObject     = (delegate* unmanaged < BaseClip*, Actor >)hooks.SigScanner.ScanText(Sigs.BaseClipGetGameObject);
+        if (_clipGameObject is null)
+            throw new Exception($"Could not scan address {Sigs.BaseClipGetGameObject}.");
+
+        Task = hooks.CreateHook<Delegate>("Get Clip Resources", Sigs.BaseClipGetClipResources, Detour,
+            !HookOverrides.Instance.Animation.GetClipResources);
+    }
+
+    public delegate nint Delegate(BaseClip* clip);
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    private nint Detour(BaseClip* clip)
+    {
+        if (clip is null)
+            return Task.Result!.Original(clip);
+
+        var gameObject = _clipGameObject(clip);
+        var data       = _collectionResolver.IdentifyCollection(gameObject.AsObject, true);
+        if (!data.Valid)
+        {
+            var ret2 = Task.Result!.Original(clip);
+            Penumbra.Log.Excessive($"[Get Clip Resources] Invoked on {(nint)clip:X} -> {ret2:X}.");
+            return ret2;
+        }
+
+        var last = _state.SetSoundData(data);
+        _crashHandler.LogAnimation(data.AssociatedGameObject, data.ModCollection, AnimationInvocationType.ScheduleClipUpdate);
+        var ret = Task.Result!.Original(clip);
+        _state.RestoreAnimationData(last);
+        Penumbra.Log.Excessive(
+            $"[Get Clip Resources] Invoked on {(nint)clip:X} for {data.AssociatedGameObject:X} ({data.ModCollection}) -> {ret:X}.");
+        return ret;
+    }
+}
