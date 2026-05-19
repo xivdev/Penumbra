24d4ad81e07d26b1afa8610dd1ee6a413a3b3676
Ottermandias <nickroederspam+git1@gmail.com>
2026-05-19 17:10:24 +0200

    Move all references to ImageSharp except for materialexporter to CustomBitmap.

--- Penumbra/Import/Textures/RgbaPixelData.cs
+++ Penumbra/Import/Textures/RgbaPixelData.cs
@@ -1,12 +1,8 @@
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-
 namespace Penumbra.Import.Textures;
 
 public readonly record struct RgbaPixelData(int Width, int Height, byte[] PixelData)
 {
-    public static readonly RgbaPixelData Empty = new(0, 0, Array.Empty<byte>());
+    public static readonly RgbaPixelData Empty = new(0, 0, []);
 
     public (int Width, int Height) Size
         => (Width, Height);
@@ -15,23 +11,11 @@ public readonly record struct RgbaPixelData(int Width, int Height, byte[] PixelD
         : this(size.Width, size.Height, pixelData)
     { }
 
-    public Image<Rgba32> ToImage()
-        => Image.LoadPixelData<Rgba32>(PixelData, Width, Height);
+    public CustomBitmap ToImage()
+        => CustomBitmap.FromPixelData(this);
 
     public RgbaPixelData Resize((int Width, int Height) size)
-    {
-        if (Width == size.Width && Height == size.Height)
-            return this;
-
-        var result = new RgbaPixelData(size, NewPixelData(size));
-        using (var image = ToImage())
-        {
-            image.Mutate(ctx => ctx.Resize(size.Width, size.Height, KnownResamplers.Lanczos3));
-            image.CopyPixelDataTo(result.PixelData);
-        }
-
-        return result;
-    }
+        => CustomBitmap.Resize(this, size);
 
     public static byte[] NewPixelData((int Width, int Height) size)
         => new byte[size.Width * size.Height * 4];
