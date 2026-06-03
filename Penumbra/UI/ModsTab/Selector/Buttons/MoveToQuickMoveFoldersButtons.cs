864af6f412ec0cd48c1ba3b6603d5746df96eb36
Ottermandias <nickroederspam+git1@gmail.com>
2026-06-03 16:15:00 +0200

    Fix Move to Quick Folder for selection.

--- Penumbra/UI/ModsTab/Selector/Buttons/MoveToQuickMoveFoldersButtons.cs
+++ Penumbra/UI/ModsTab/Selector/Buttons/MoveToQuickMoveFoldersButtons.cs
@@ -8,77 +8,39 @@ public sealed class MoveToQuickMoveFoldersButtons(ModFileSystemDrawer drawer) : 
     public override ReadOnlySpan<byte> Label(in IFileSystemData data)
         => throw new NotImplementedException();
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void MoveDataEntry(in IFileSystemData data, ReadOnlySpan<char> currentName, string currentPath, string folder, Im.IdDisposable id)
+    {
+        if (folder.Length is 0)
+            return;
+
+        var targetPath = $"{folder}/{currentName}";
+        if (drawer.FileSystem.Equal(currentPath, targetPath))
+            return;
+
+        id.PushNext();
+        if (Im.Menu.Item($"Move to {folder}"))
+        {
+            if (data.Selected)
+                foreach (var path in drawer.FileSystem.Selection.OrderedNodes)
+                    drawer.FileSystem.RenameAndMoveWithDuplicates(path, $"{folder}/{path.Name}");
+            else
+                drawer.FileSystem.RenameAndMoveWithDuplicates(data, targetPath);
+        }
+
+        Im.Tooltip.OnHover("Move the selected objects to a previously set-up quick move location, if possible."u8);
+        id.Pop();
+    }
+
     public override bool DrawMenuItem(in IFileSystemData data)
     {
         var       currentName = data.Name;
         var       currentPath = data.FullPath;
         using var id          = new Im.IdDisposable();
-        if (drawer.Config.QuickMoveFolder1.Length > 0)
-        {
-            id.Push(0);
-            var targetPath = $"{drawer.Config.QuickMoveFolder1}/{currentName}";
-            if (!drawer.FileSystem.Equal(currentPath, targetPath))
-            {
-                if (Im.Menu.Item($"Move to {drawer.Config.QuickMoveFolder1}"))
-                {
-                    foreach (var path in drawer.FileSystem.Selection.OrderedNodes)
-                    {
-                        if (path != data)
-                            drawer.FileSystem.RenameAndMoveWithDuplicates(path, $"{drawer.Config.QuickMoveFolder1}/{path.Name}");
-                    }
 
-                    drawer.FileSystem.RenameAndMoveWithDuplicates(data, targetPath);
-                }
-                Im.Tooltip.OnHover("Move the selected objects to a previously set-up quick move location, if possible."u8);
-            }
-
-            id.Pop();
-        }
-
-        if (drawer.Config.QuickMoveFolder2.Length > 0)
-        {
-            id.Push(1);
-            var targetPath = $"{drawer.Config.QuickMoveFolder2}/{currentName}";
-            if (!drawer.FileSystem.Equal(currentPath, targetPath))
-            {
-                if (Im.Menu.Item($"Move to {drawer.Config.QuickMoveFolder2}"))
-                {
-                    foreach (var path in drawer.FileSystem.Selection.OrderedNodes)
-                    {
-                        if (path != data)
-                            drawer.FileSystem.RenameAndMoveWithDuplicates(path, $"{drawer.Config.QuickMoveFolder2}/{path.Name}");
-                    }
-
-                    drawer.FileSystem.RenameAndMoveWithDuplicates(data, targetPath);
-                }
-                Im.Tooltip.OnHover("Move the selected objects to a previously set-up quick move location, if possible."u8);
-            }
-
-            id.Pop();
-        }
-
-        if (drawer.Config.QuickMoveFolder3.Length > 0)
-        {
-            id.Push(2);
-            var targetPath = $"{drawer.Config.QuickMoveFolder3}/{currentName}";
-            if (!drawer.FileSystem.Equal(currentPath, targetPath))
-            {
-                if (Im.Menu.Item($"Move to {drawer.Config.QuickMoveFolder3}"))
-                {
-                    foreach (var path in drawer.FileSystem.Selection.OrderedNodes)
-                    {
-                        if (path != data)
-                            drawer.FileSystem.RenameAndMoveWithDuplicates(path, $"{drawer.Config.QuickMoveFolder3}/{path.Name}");
-                    }
-
-                    drawer.FileSystem.RenameAndMoveWithDuplicates(data, targetPath);
-                }
-                Im.Tooltip.OnHover("Move the selected objects to a previously set-up quick move location, if possible."u8);
-            }
-
-            id.Pop();
-        }
-
+        MoveDataEntry(data, currentName, currentPath, drawer.Config.QuickMoveFolder1, id);
+        MoveDataEntry(data, currentName, currentPath, drawer.Config.QuickMoveFolder2, id);
+        MoveDataEntry(data, currentName, currentPath, drawer.Config.QuickMoveFolder3, id);
         return false;
     }
 }
