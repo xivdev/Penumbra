50c1556c50346607160476f96e85b498ba9b80f6
Ottermandias <nickroederspam+git1@gmail.com>
2026-06-25 01:19:37 +0200

    Use node tester in condition drawer for the moment.

--- Penumbra/UI/ModsTab/ConditionEditPopup.cs
+++ Penumbra/UI/ModsTab/ConditionEditPopup.cs
@@ -1,7 +1,7 @@
 using ImSharp;
+using ImSharp.ImNodes;
 using Luna;
 using Penumbra.Mods.Groups;
-using Penumbra.Mods.Settings;
 using Penumbra.Mods.SubMods;
 
 namespace Penumbra.UI.ModsTab;
@@ -19,10 +19,20 @@ public sealed class ConditionEditPopup(ModGroupConditionDrawer drawer) : ObjectE
         if (Current is not IModObject obj)
             return;
 
-        var cursor = Im.Cursor.Position;
-        Im.ScaledDummy(640);
-        Im.Cursor.Position = cursor;
-        if (drawer.Draw(obj.Condition, new ModSettingContext(obj.Mod, ModSettings.Empty, obj), out var newCondition))
-            obj.Condition = newCondition;
+        ImNodes.Tester.Show("editor"u8);
+
+        //Im.ScaledDummy(640);
+        //Im.Cursor.Position = cursor;
+        //if (drawer.Draw(obj.Condition, new ModSettingContext(obj.Mod, ModSettings.Empty, obj), out var newCondition))
+        //    obj.Condition = newCondition;
     }
+
+    protected override void PrePopup()
+    {
+        Im.Window.SetNextSize(ImEx.ScaledVector(640, 640));
+        ImStyleDouble.WindowPadding.Push(Vector2.Zero);
+    }
+
+    protected override void PostPopup()
+        => Im.StyleDisposable.PopUnsafe();
 }
